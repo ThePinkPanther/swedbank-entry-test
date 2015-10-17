9c1a8a7a231b479ad1ef52d57f3e83c2652bbcaf
ben <rbjacikevicius@gmail.com>
2015-10-17 15:43:03 +0300

    Main class

---
+++ src/main/java/com/swedbank/entry_test/EntryTest.java
@@ -0,0 +1,15 @@
+package com.swedbank.entry_test;
+
+import java.util.Arrays;
+
+/**
+ * @author ben
+ * @version 1.0
+ */
+public class EntryTest {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+    }
+
+}
