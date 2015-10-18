5faac6c04735aaf4905be862bb61bab6e91e23d8
ben <rbjacikevicius@gmail.com>
2015-10-18 22:43:45 +0300

    Renamed and updated CSV entry decoder

--- src/main/java/com/swedbank/entry_test/util/CustomDecathlonCsvEntryDecoder.java
+++ src/main/java/com/swedbank/entry_test/util/CustomDecathlonCsvEntryDecoder.java
@@ -3,31 +3,48 @@ package com.swedbank.entry_test.util;
 
 import com.swedbank.entry_test.util.data.DecathlonResultEntry;
 
+import java.text.SimpleDateFormat;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author ben
  * @version 1.0
  */
-public class DecathlonCsvEntryDecoder implements DecathlonEntryDecoder {
+public class CustomDecathlonCsvEntryDecoder implements DecathlonEntryDecoder {
 
     private final static int CSV_COL_NUM = 11;
     private final String separator;
 
-    public DecathlonCsvEntryDecoder() {
+    public CustomDecathlonCsvEntryDecoder() {
         this(";");
     }
 
-    public DecathlonCsvEntryDecoder(String separator) {
+    public CustomDecathlonCsvEntryDecoder(String separator) {
         this.separator = separator;
     }
 
+    /**
+     * Produces decathlon entry from a CSV file line
+     * @param str CSV file line
+     * @return
+     * @throws DecodingException
+     */
     @Override
     public DecathlonResultEntry decode(String str) throws DecodingException {
         String[] columns = str.trim().split(separator);
         if (columns.length != CSV_COL_NUM) {
-            throw new DecodingException("Given line has wrong number of fields ("+CSV_COL_NUM+" " +
-                    "expected)");
+            throw new DecodingException(
+                    "Given line has wrong number of fields (" + CSV_COL_NUM + " " + "expected)");
         }
         try {
+
+            // Parse the last fields that holds minutes
+            String[] marathonTime = columns[10].split("\\.",2);
+            if (marathonTime.length != 2)  {
+                throw new DecodingException(
+                        "oneThousandFiveHoundedMetresRunTime format is expected to be mm:ss:SS");
+            }
+
             return new DecathlonResultEntry(
                     columns[0],
                     Double.parseDouble(columns[1]),
@@ -39,8 +56,8 @@ public class DecathlonCsvEntryDecoder implements DecathlonEntryDecoder {
                     Double.parseDouble(columns[7]),
                     Double.parseDouble(columns[8]),
                     Double.parseDouble(columns[9]),
-                    Double.parseDouble(columns[10]),
-                    null);
+                    Double.parseDouble(marathonTime[1])+Integer.parseInt(marathonTime[0])*60,
+                    null, null);
         } catch (NumberFormatException e) {
             throw new DecodingException(e.getMessage());
         }
